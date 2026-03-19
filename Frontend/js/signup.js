1225ede01945d82edb1db6be1a5ca9624f049812
AAYUSKA RAI <aayuskaaraii@office.uc.ac.kr>
2026-03-19 15:48:12 +0000

    Updated email validification feature

--- Frontend/js/signup.js
+++ Frontend/js/signup.js
@@ -1,5 +1,12 @@
 document.getElementById("signupForm").addEventListener("submit", function(e) {
    e.preventDefault();
 
-   // validations will be added by team
+   const email = document.getElementById("email").value.trim();
+const emailPattern = /^[a-zA-Z0-9._%+-]+@office\.uc\.ac\.kr$/;
+
+if (!emailPattern.test(email)) {
+   alert("Email must be @office.uc.ac.kr");
+   return;
+}
+
 });
