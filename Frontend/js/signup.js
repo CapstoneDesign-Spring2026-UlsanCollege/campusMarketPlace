96d6081aaffe19dea55c3724cf6e22ce6e6f5bbc
gayatri704 <kumari@office.uc.ac.kr>
2026-03-20 16:06:34 +0900

    adjusting signup page

--- Frontend/js/signup.js
+++ Frontend/js/signup.js
@@ -1,5 +1,14 @@
 document.getElementById("signupForm").addEventListener("submit", function(e) {
    e.preventDefault();
+   if (firstName === "") {
+   alert("First name is required");
+   return;
+}
+
+if (lastName === "") {
+   alert("Last name is required");
+   return;
+}
 
    const email = document.getElementById("email").value.trim();
 const emailPattern = /^[a-zA-Z0-9._%+-]+@office\.uc\.ac\.kr$/;
@@ -18,16 +27,6 @@ if (!emailPattern.test(email)) {
 }
    const firstName = document.getElementById("firstName").value.trim();
 const lastName = document.getElementById("lastName").value.trim();
-
-if (firstName === "") {
-   alert("First name is required");
-   return;
-}
-
-if (lastName === "") {
-   alert("Last name is required");
-   return;
-}
 
  alert("Signup successful!");
 
